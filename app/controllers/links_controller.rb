4632364f7c80f46e47cac7ea5d81e137bf643f3f
Brian Heim <bheim67@gmail.com>
2017-01-08 15:39:54 -0700

    now able to send links with updated read count to hot reads db from URLockbox

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,6 +4,13 @@ class LinksController < ApplicationController
   end
 
   def create
-
+    link = Link.find_or_create_by(link_params)
+      link.read_count += 1
+      link.save
   end
+
+  private
+    def link_params
+      params.permit(:title, :url)
+    end
 end
